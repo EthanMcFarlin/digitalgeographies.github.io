4a77c964c6f01fda64e37cbfbed8fd4e56e26e68
McFarlin <34318060+EthanMcFarlin@users.noreply.github.com>
2024-07-25 01:07:17 -0400

    Specific value reporting in tooltips / interface updates

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZXRoYW5tY2ZhcmxpbiIsImEiOiJjbHl6NGo0OG8wbm5iMmxwdnVvNWl6dHUwIn0.pyb_HA0qJwJ9jZkrmlKcqA';
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v11',
+    style: 'mapbox://styles/mapbox/light-v11',
     projection: 'albers',
     zoom: 4,
     minZoom: 3,
@@ -65,43 +65,58 @@ let scaleDictionary = [
     {
         name: "SPL_THEMES",
         min: 0.99,
-        max: 13.57
-
+        max: 13.57,
+        avg: 7.674277,
+        desc: "Sum of series themes (1: Socioeconomic Status, 2: Household Characteristics, 3: Racial and Ethnic Minority Status, 4: Housing Type / Transportation)"
     },
     {
         name: "E_HBURD",
         min: 0,
-        max: 1644
+        max: 1644,
+        avg: 399.3317,
+        desc: "Housing cost burdened occupied housing units with annual income less than $75,000 (30%+ of income spent on housing costs) estimate, 2016-2020 ACS"
     },
     {
         name: "E_DISABL",
         min: 0,
-        max: 1552
+        max: 1552,
+        avg: 489.3169,
+        desc: "Civilian noninstitutionalized population with a disability estimate, 2014-2018 ACS"
     },
     {
         name: "EPL_POV150",
         min: 0,
-        max: 0.9995
+        max: 0.9995,
+        avg: 0.4998178,
+        desc: "Percentile percentage of persons below 150% poverty estimate"
     },
     {
         name: "EP_UNINSUR",
         min: 0,
-        max: 100
+        max: 100,
+        avg: 8.805836,
+        desc: "Percentage uninsured in the total civilian noninstitutionalized population estimate, 2016-2020 ACS"
     },
     {
         name: "E_MINRTY",
         min: 0,
-        max: 12602
+        max: 12602,
+        avg: 1559.891,
+        desc: "Minority (Hispanic or Latino (of any race); Black and African American, Not Hispanic or Latino; American Indian and Alaska Native, Not Hispanic or Latino; Asian, Not Hispanic or Latino; Native Hawaiian and Other Pacific Islander, Not Hispanic or Latino; Two or More Races, Not Hispanic or Latino; Other Races, Not Hispanic or Latino) estimate, 2016-2020 ACS"
     },
     {
         name: "E_AFAM",
         min: 0,
-        max: 7843
+        max: 7843,
+        avg: 478.1086,
+        desc: "Adjunct variable - Black/African American, not Hispanic or Latino persons estimate, 2016-2020 ACS"
     },
     {
         name: "E_HISP",
         min: 0,
-        max: 7334
+        max: 7334,
+        avg: 710.9013,
+        desc: "Adjunct variable – Hispanic or Latino persons estimate, 2016-2020 ACS"
     },
 ]
 
@@ -143,7 +158,7 @@ function updateCircles() {
 // On Load
 // ********
 
-map.on('load', () => {
+map.on('style.load', () => {
 
     map.addSource('centers', {
         type: 'geojson',
@@ -185,6 +200,20 @@ map.on('load', () => {
 });
 
 // ********
+// Calculate Percent Difference
+// ********
+
+function calculatePercentDifference(inputValue, inputVariable) {
+    let nationalMean = scaleDictionary[chosenSVIMeasure].avg;
+
+    let diffAbs = inputValue - nationalMean;
+    let diffRel = diffAbs / nationalMean;
+    let diffPer = diffRel * 100;
+
+    return diffPer.toFixed(2);
+}
+
+// ********
 // On Click: Points
 // ********
 
@@ -195,11 +224,26 @@ map.on('click', 'centers-layer', (e) => {
     const description = e.features[0].properties.Concatenated_Address;
     const property = e.features[0].properties[chosenProperty];
     const classification = e.features[0].properties.Classification;
+    const dictDesc = scaleDictionary[chosenSVIMeasure].desc;
+
+    let relevantMean = scaleDictionary[chosenSVIMeasure].avg;
+    relevantMean = relevantMean.toFixed(2);
+
+    let classificationColor;
 
     if (['mercator', 'equirectangular'].includes(map.getProjection().name)) {
         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
+    }
+
+    let percentDifference = calculatePercentDifference(property, chosenProperty);
+    let parityStatus;
+
+    if (percentDifference > 0) {
+        parityStatus = "greater";
+    } else {
+        parityStatus = "less";
     }
 
     new mapboxgl.Popup({className: "info-container" })
@@ -211,19 +255,39 @@ map.on('click', 'centers-layer', (e) => {
         //     + chosenProperty + ": " + property + "<br><br>"
         //     + "</span>"
         // )
+        // <i id="arrow" class="fa-solid fa-down-left-and-up-right-to-center"></i>
         .setHTML(
             "<h1 class='popup-header'>" + description + "</h1>" +
             // "<p>" + "<span class='popup-classification'>" + classification + "</span>" + "</p>" +
             "<p>" + "<span id='popup-classification'>" + classification + "</span>" + "</p>" +
-            "<p class='popup-property'>" + "<span class='popup-emphasis'>" + chosenProperty + ": " + "</span>" + "<span class='popup-light'>" + property + "</span>" + "</p>"
+            "<p class='popup-property'>" + "<span class='popup-emphasis'>" + chosenProperty + ": " + "</span>" + "<span class='popup-light'>" + property + "</span>" + "</p>" +
+    "<p class='popup-relValue'><span id='colorCorrespondingly'>" + "<i id='colorArrow' class='fa-solid'></i>" + " " + percentDifference + "% " + parityStatus + "</span>" + " than the national average of " + "<span id='relMean'>" + relevantMean + "</span>" + " for all U.S. Census tracts in 2020." + "</p>" +
+            "<p class='popup-desc' id='descBorder'>" + "<span id='censusBureauDesc'>" + "Variable Description: " + "</span>" + "<span class='popup-small'>" + dictDesc + "</span>" + "</p>"
         )
         .addTo(map);
 
     if (classification == "Warehouse") {
         document.getElementById("popup-classification").classList = "Warehouse";
+        classificationColor = "#f2cbd0";
     } else {
         document.getElementById("popup-classification").classList = "DC";
+        classificationColor = "#e8deff";
     }
+
+    let colorElement = document.getElementById("colorCorrespondingly");
+    let colorArrowElement = document.getElementById("colorArrow");
+
+    if (percentDifference > 0) {
+        colorElement.style.color = "#e64956";
+        colorArrowElement.classList.add("fa-arrow-up")
+    } else {
+        colorElement.style.color = "#518c5b";
+        colorArrowElement.classList.add("fa-arrow-down")
+    }
+
+    let descBorderElement = document.getElementById("descBorder");
+    descBorderElement.style.borderLeftColor = classificationColor;
+
 });
 
 map.on('mouseenter', 'centers-layer', () => {
@@ -263,6 +327,50 @@ function minimize() {
         arrow.classList.remove("fa-up-right-and-down-left-from-center")
     }
 
+    console.log(currentColorStatus, menu_status);
 
+    // while (currentColorStatus && menu_status) {
+    //     document.getElementById("rounded-parent").classList.backgroundColor = "#fff";
+    //     document.getElementById("minimize").style.color = "black";
+    // }
+
+    // if (currentColorStatus && menu_status) {
+    //     document.getElementById("rounded-parent").classList.backgroundColor = "#fff";
+    //     document.getElementById("minimize").style.color = "black";
+    //
+    // } else if ( (currentColorStatus === true) && (menu_status === false)) {
+    //     document.getElementById("rounded-parent").style.backgroundColor = "#1a1a1a";
+    //     document.getElementById("minimize").style.color = "white";
+    // } else if ( (currentColorStatus === false) && (menu_status === false)) {
+    //     // document.getElementById("rounded-parent").style.backgroundColor = "#fff";
+    // }
+
+
+
+}
+
+// ********
+// Dynamic Color Scheme
+// ********
+
+let currentColorStatus = false;
+
+function updateColorScheme() {
+    currentColorStatus = !currentColorStatus;
+
+    let elementsToChange = ["SVI-header", "EJ-header", "legend-header", "color-header", "rounded-parent",
+        "minimize", "arrow", "SVI-variables", "EJ-variables", "legend-1", "legend-2"];
+
+    if (currentColorStatus) {
+        elementsToChange.forEach(function (element) {
+            document.getElementById(element).classList.add("dark");
+            map.setStyle('mapbox://styles/mapbox/' + "dark-v11");
+        })
+    } else {
+        elementsToChange.forEach(function (element) {
+            document.getElementById(element).classList.remove("dark");
+            map.setStyle('mapbox://styles/mapbox/' + "light-v11");
+        })
+    }
 
 }
